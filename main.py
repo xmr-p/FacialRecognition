643948aa05edd083230f11e745c644a59b81b5ea
xmr-p <150638569+xmr-p@users.noreply.github.com>
2023-11-22 14:43:39 -0600

    Fix: main.py
    Fixing errors in the splitting function

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import cv2
 
-def splitToFrames(videosPath, frameNum):
-	for i in range(17):
-		capture = cv2.VideoCapture(videosPath + str(i))
+vPath = "videos/"
+fPath = "frames/"
+extension = ".mp4"
+nVids = 17
+
+def splitToFrames(videosPath, framesPath, frameNum, videosExt, numVids):
+	for i in range(numVids):
+		frameNum = 0
+		capture = cv2.VideoCapture(videosPath + str(i) + videosExt)
 		while True:
 			success, frame = capture.read()
 			if success:
-				cv2.imwrite(f"frames/vid-{i}frame-{frameNum}.jpg", frame)
+				cv2.imwrite(f"{framesPath}vid-{i}-frame-{frameNum}.jpg", frame)
 			else:
 				break
-			frameNr += 1
+			frameNum += 1
+		print(f"[1]- Video {i}: DONE")
 		capture.release()
+	print("[1]- Splitting: DONE")
 
 def main():
-	splitToFrames("videos/", 0)
+	splitToFrames(vPath, fPath, 0, extension, nVids)
 main()
