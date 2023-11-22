6d77b1af601d67b9960c8a8b6d0269afae44d913
xmr-p <150638569+xmr-p@users.noreply.github.com>
2023-11-22 15:27:25 -0600

    Add: Image to grayscale function
    Added a function in main.py to turn the frames from rgb to grayscale

--- main.py
+++ main.py
@@ -2,24 +2,39 @@ import cv2
 
 vPath = "videos/"
 fPath = "frames/"
+gPath = "grayscale/"
 extension = ".mp4"
-nVids = 17
+nVids = 3
+nFrames = 0
 
 def splitToFrames(videosPath, framesPath, frameNum, videosExt, numVids):
 	for i in range(numVids):
-		frameNum = 0
 		capture = cv2.VideoCapture(videosPath + str(i) + videosExt)
 		while True:
 			success, frame = capture.read()
 			if success:
-				cv2.imwrite(f"{framesPath}vid-{i}-frame-{frameNum}.jpg", frame)
+				cv2.imwrite(f"{framesPath}{frameNum}.jpg", frame)
 			else:
 				break
 			frameNum += 1
-		print(f"[1]- Video {i}: DONE")
+		print(f"   - Video {i}: DONE")
 		capture.release()
-	print("[1]- Splitting: DONE")
+	print("[1]- SPLITTING: DONE")
+	return frameNum
+
+def imgToGrayscale(frame, grayscalePath, indx):
+	img = cv2.imread(frame)
+	grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	cv2.imwrite(f"{grayscalePath}{indx}.jpg", grayImg)
+	print(f"   - Frame {indx} grayscale: DONE")
+	
 
 def main():
-	splitToFrames(vPath, fPath, 0, extension, nVids)
+	print("[1]- SPLITTING: STARTING...")
+	nFrames = splitToFrames(vPath, fPath, 0, extension, nVids)
+	print("[2]- GRAYSCALING: STARTING...")
+	for i in range(nFrames):
+		imgToGrayscale((fPath + str(i) + ".jpg"), gPath, i)
+	print("[2]- GRAYSCALING: DONE")  
+		
 main()
