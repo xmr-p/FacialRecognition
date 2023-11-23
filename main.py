89ca0e247a94da9dfce9a9d0a30a59a7332c928d
xmr-p <150638569+xmr-p@users.noreply.github.com>
2023-11-22 21:48:26 -0600

    Add: Face extraction function
    Add a function in main.py that extracts anything that looks like a face, and puts it into a folder

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import cv2
 vPath = "videos/"
 fPath = "frames/"
 gPath = "grayscale/"
+facesP = "faces/"
 extension = ".mp4"
-nVids = 3
-nFrames = 0
+nVids = 7
+nFrames = 1109
 
 def splitToFrames(videosPath, framesPath, frameNum, videosExt, numVids):
 	for i in range(numVids):
@@ -27,8 +28,18 @@ def imgToGrayscale(frame, grayscalePath, indx):
 	grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	cv2.imwrite(f"{grayscalePath}{indx}.jpg", grayImg)
 	print(f"   - Frame {indx} grayscale: DONE")
-	
 
+def detectFaces():
+	faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+	for i in range(nFrames):
+		gray = cv2.imread(gPath + str(i) + ".jpg")
+		faces = faceCascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30)) 
+		for (x, y, w, h) in faces:
+			cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+			roiColor = gray[y:y + h, x:x + w]
+		status = cv2.imwrite(f"{facesP}{i}.jpg", roiColor)
+		print(f"[3]- Image {i}.jpg written to filesystem: ", status)
+	
 def main():
 	print("[1]- SPLITTING: STARTING...")
 	nFrames = splitToFrames(vPath, fPath, 0, extension, nVids)
@@ -36,5 +47,8 @@ def main():
 	for i in range(nFrames):
 		imgToGrayscale((fPath + str(i) + ".jpg"), gPath, i)
 	print("[2]- GRAYSCALING: DONE")  
+	print("[3]- FACE DETECTION: STARTING...")
+	detectFaces()
+	print("[3]- FACE DETECTION: DONE")
 		
 main()
